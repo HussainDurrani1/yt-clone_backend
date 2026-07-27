a93dd6e77bbc6e6109d97aee561bad74d55b2aa0
Hussain Durrani <hussaindurrani85@gmail.com>
2026-07-27 22:49:18 +0500

    add like, playlist, tweet, and comment model

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -141,7 +141,7 @@ const logoutUser = asyncHandler(async (req, res) => {
 
     await User.findByIdAndUpdate(
         req.user._id,
-        { $set: { refreshToken: undefined } },
+        { $unset: { refreshToken: 1 } },
         { returnDocument: 'after' }
     );
 
